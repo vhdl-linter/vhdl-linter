103910fa3628540a6f82be874fc61a2e2282f8fb
schelten <niklas.schelten@hhi.fraunhofer.de>
2018-11-01 15:57:54 +0100

    rm dist

--- dist/parser/parser-position.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ParserPosition {
-    constructor() {
-        this.i = 0;
-    }
-}
-exports.ParserPosition = ParserPosition;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicGFyc2VyLXBvc2l0aW9uLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vbGliL3BhcnNlci9wYXJzZXItcG9zaXRpb24udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxNQUFhLGNBQWM7SUFFekI7UUFDRSxJQUFJLENBQUMsQ0FBQyxHQUFHLENBQUMsQ0FBQztJQUNiLENBQUM7Q0FDRjtBQUxELHdDQUtDIn0=
