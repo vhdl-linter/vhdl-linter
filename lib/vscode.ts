0327c376386ef105b3f298032f23107ddfde8a3e
Anton Schulte <mail@anton-schulte.de>
2019-10-22 19:24:45 +0200

    debug port

--- lib/vscode.ts
+++ lib/vscode.ts
@@ -15,7 +15,7 @@ export function activate(context: ExtensionContext) {
   let serverModule = require.resolve('./language-server');
   // The debug options for the server
   // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-  let debugOptions = { execArgv: ['--nolazy', '--inspect'] };
+  let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
 
   // If the extension is launched in debug mode then the debug server options are used
   // Otherwise the run options are used
