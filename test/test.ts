5e8c8b3328ffdec67234f58cf775fa80d8bdc094
anton schulte <mail@anton-schulte.de>
2022-10-08 10:53:10 +0200

    added minimal CI (currently fails)

---
+++ test/test.ts
@@ -0,0 +1,25 @@
+import { VhdlLinter } from '../lib/vhdl-linter';
+import { env, cwd } from 'process';
+import { readdirSync, readFileSync } from 'fs';
+import { join } from 'path';
+import { ProjectParser } from '../lib/project-parser';
+
+(async () => {
+
+  const files = readdirSync(join(cwd(), 'ieee2008')).filter(file => file.match(/\.vhdl?$/i)).map(file => join(cwd(), 'ieee2008', file));
+  const messages = [];
+  for (const file of files) {
+    const text = readFileSync(file, {encoding: 'utf8'});
+    const projectParser = new ProjectParser([join(cwd(), 'ieee2008')], '');
+    const vhdlLinter = new VhdlLinter(file, text,  projectParser);
+    await vhdlLinter.checkAll();
+    if (vhdlLinter.messages.length > 0) {
+      messages.push({
+        file: file,
+        messages: vhdlLinter.messages
+      });
+    }
+  }
+  console.log(JSON.stringify(messages, null, 2));
+  process.exit(messages.length);
+})();
