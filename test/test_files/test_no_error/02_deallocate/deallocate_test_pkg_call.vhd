18278d6f311a64869cfbe57050f9196610734a42
Anton Schulte <anton.schulte@pts.space>
2022-10-13 13:00:45 +0200

    added minimal failure case for deallocate

---
+++ test/test_files/test_no_error/02_deallocate/deallocate_test_pkg_call.vhd
@@ -0,0 +1,21 @@
+use work.deallocate_test_pkg_def.all;
+
+package deallocate_test_pkg_call is
+  function test_func
+    return boolean;
+
+end package;
+package body deallocate_test_pkg_call is
+
+  function test_func
+
+    return boolean is
+    variable test : test_type;
+  begin
+    test := new test_type;
+    deallocate (test);
+
+  end function;
+
+
+end package;
---
+++ test/test_files/test_no_error/02_deallocate/deallocate_test_pkg_def.vhd
@@ -0,0 +1,5 @@
+package deallocate_test_pkg_def is
+  type test_type is access integer;
+
+
+end package;
