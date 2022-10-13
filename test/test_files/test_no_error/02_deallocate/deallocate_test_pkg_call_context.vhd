07ba57cdb4911655885dd059660007536bff5dd3
Anton Schulte <anton.schulte@pts.space>
2022-10-13 13:49:30 +0200

    added context test

---
+++ test/test_files/test_no_error/02_deallocate/deallocate_test_pkg_call_context.vhd
@@ -0,0 +1,21 @@
+context work.test_context;
+
+package deallocate_test_pkg_call is
+  function test_func
+    return boolean;
+
+end package;
+package body deallocate_test_pkg_call is
+
+  function test_func
+
+    return boolean is
+    variable test : test_type;
+  begin
+    test := new test_type;
+    deallocate (test);
+
+  end function;
+
+
+end package;
---
+++ test/test_files/test_no_error/02_deallocate/deallocate_test_pkg_context.vhd
@@ -0,0 +1,3 @@
+context test_context is
+  use work.deallocate_test_pkg_def.all;
+end context;
