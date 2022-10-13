68a9a479f61e7ced2ce67f2937be36e7435da0a7
g0t00 <www.gollum@gmail.com>
2022-10-13 14:49:52 +0200

    Fix VHDL error in Use clause handling branch (#72)
    * added minimal failure case for deallocate
    
    * Use clauses should work across files with package body and architecture
    
    * added context test
    
    * Package context working across files
    
    * broken test case
    
    * valid vhdl
    
    * validate vhdl rev2
    
    * valid vhdl rev3
    
    * vhdl working rev4
    
    * vhdl working rev99
    
    * vhdl valid rev100
    
    * vhdl valid rev101
    
    * vhdl valid rev102
    
    Co-authored-by: Anton Schulte <anton.schulte@pts.space>

--- test/test_files/test_no_error/02_deallocate/deallocate_test_pkg_call_context.vhd
+++ test/test_files/test_no_error/02_deallocate/deallocate_test_pkg_call_context.vhd
@@ -12,10 +12,9 @@ package body deallocate_test_pkg_call_context is
     return boolean is
     variable test : test_type;
   begin
-    test := new test_type;
     deallocate (test);
 
   end function;
 
 
-end package;
+end package body;
