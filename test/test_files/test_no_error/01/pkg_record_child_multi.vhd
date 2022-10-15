63ac75611e9ce379bf6889f2425c47a93fa5c5b2
Nik-Sch <mail@niklas-schelten.de>
2022-10-15 22:49:33 +0200

    fixed parsing of record children

---
+++ test/test_files/test_no_error/01/pkg_record_child_multi.vhd
@@ -0,0 +1,11 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package pkg_record_child_multi is
+  type t_testData is record
+    element, this_also_exists: integer;
+  end record t_testData;
+
+  constant apfel: integer := this_also_exists;
+end package;
