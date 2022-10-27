065654ea0dd8980118c0e2cb0404805cf5763570
Nik-Sch <mail@niklas-schelten.de>
2022-10-27 15:56:20 +0200

    fix components

---
+++ test/test_files/test_no_error/01/test_component.vhd
@@ -0,0 +1,12 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package test_component is
+
+  component test_else_generate is
+    port (
+      i_clk : in std_ulogic
+    );
+  end component test_else_generate;
+end package;
