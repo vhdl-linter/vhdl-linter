3cb816ff2914684f809d4fdd431493b6128deb5b
Your Name <you@example.com>
2022-11-09 17:56:39 +0100

    Empty file handling

--- test/vunit_compile_test_no_error.py
+++ test/vunit_compile_test_no_error.py
@@ -5,8 +5,10 @@ vu = VUnit.from_argv(compile_builtins=False)
 for dir in Path(__file__).parent.joinpath('test_files', 'test_no_error').iterdir():
   print("\n\nentering folder: %s"%(dir.absolute()))
   # Create VUnit instance by parsing command line arguments
-
-  vu.add_library(f'test_{dir.name}')
-  vu.add_source_files(dir.absolute() / '*.vhd', f'test_{dir.name}', vhdl_standard='2008')
+  libraryname = f'test_{dir.name}'
+  if dir.name == 'OSVVM':
+    libraryname = 'OSVVM';
+  vu.add_library(libraryname)
+  vu.add_source_files(dir.absolute() / '*.vhd', libraryname, vhdl_standard='2008')
 
 vu.main()
