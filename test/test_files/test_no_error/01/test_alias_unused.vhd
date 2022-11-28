a9cf5e9ade7828d04a7fc5e21f42c8effe384b4d
anton schulte <mail@anton-schulte.de>
2022-11-28 20:13:05 +0100

    vhdl syntax

--- test/test_files/test_no_error/01/test_alias_unused.vhd
+++ test/test_files/test_no_error/01/test_alias_unused.vhd
@@ -3,17 +3,18 @@ package test_alias_unused is
 end package;
 package body test_alias_unused is
   procedure RemoveExclude(
+    a_unused : boolean
     ) is
-    variable NewA : integer_vector;
+    variable NewA : integer_vector(0 to 5);
     alias norm_NewA : integer_vector(1 to NewA'length) is NewA;
   begin
-    norm_NewA(0) <= 0;
+    norm_NewA(0) := 0;
   end procedure RemoveExclude;
   procedure RemoveExclude(
     variable NewA : out integer_vector
     ) is
     alias norm_NewA : integer_vector(1 to NewA'length) is NewA;
   begin
-    norm_NewA(0) <= 0;
+    norm_NewA(0) := 0;
   end procedure RemoveExclude;
 end package body;
