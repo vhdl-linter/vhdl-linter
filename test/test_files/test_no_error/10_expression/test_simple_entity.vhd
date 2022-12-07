ce0dbe0716f2257d97c2902d5d5f1969b5651277
Nik-Sch <mail@niklas-schelten.de>
2022-12-07 22:29:09 +0100

    expression parser tests

--- test/test_files/test_no_error/10_expression/test_simple_entity.vhd
+++ test/test_files/test_no_error/10_expression/test_simple_entity.vhd
@@ -1,12 +1,37 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
 entity test_simple_entity is
 
 end entity;
 architecture arch of test_simple_entity is
 
-  signal a : integer;
-  signal b : integer;
-
+  signal a   : unsigned(1 downto 0);
+  signal a_i : integer;
 begin
- a <= 5 + 7;
- b <= 5 + a;
-end arch ; --
+  a    <= a;
+  a_I  <= 5 + 7;
+  a    <= 5 - a;
+  a    <= (and a) & a;
+  a    <= a nor a;
+  a    <= a and a and a;
+  a_i  <= a_i ** a_i;
+  a_i  <= a_i ** (a_i / 2);
+  a_i  <= a_i * abs a_i;
+  a_i  <= - a_i;
+  a_i  <= - a_i - a_i;
+  a(0) <= '1' when a_i /= a_i else '0';
+  a(0) <= '1' when ?? a(0)    else '0';
+  a    <= ('0', '1');
+  a    <= (0 => '0', others => '1');
+  a    <= (0 => '0', 1 => '1');
+  a    <= ("10");
+  a_i  <= + ((((((a_i + 2))))));
+  a    <= a sll 2 + 3 * 32;
+  a    <= a(to_integer(a * 2) - 3 downto 0 * 3);
+  a    <= to_unsigned(a_i, 2);
+  a    <= unsigned(std_ulogic_vector(a));
+  a(0) <= xnor a;
+  a    <= unsigned'(1 => '0');
+end arch;
