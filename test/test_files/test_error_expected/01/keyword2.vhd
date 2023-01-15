57ac111649c1b68cf030e179031840663bd4f507
g0t00 <www.gollum@gmail.com>
2023-01-15 13:55:19 +0100

    Update test/test_files/test_error_expected/01/keyword2.vhd
    Co-authored-by: Niklas <Nik-Sch@users.noreply.github.com>

--- test/test_files/test_error_expected/01/keyword2.vhd
+++ test/test_files/test_error_expected/01/keyword2.vhd
@@ -1,11 +1,7 @@
-
-
-             architecture arch of units is -- units is reserved
-
-                                        begin
-                                          a_p : process
-                                          begin
-                                            wait for 6 ns;
-                                          end process;
-
-                                        end architecture;
+architecture arch of units is -- units is reserved
+begin
+  a_p : process
+  begin
+    wait for 6 ns;
+  end process;
+end architecture;
