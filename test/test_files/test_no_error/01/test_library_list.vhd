8c3252a90e608ef1abc98039d237a2000de3c0e8
Nik-Sch <niklas.schelten@hhi.fraunhofer.de>
2023-01-24 12:08:10 +0100

    added support for library clause with multiple libraries

---
+++ test/test_files/test_no_error/01/test_library_list.vhd
@@ -0,0 +1,9 @@
+library ieee, std;
+use ieee.std_logic_1164.all;
+use std.textio.all;
+
+entity test_library_list is
+end entity;
+architecture arch of test_library_list is
+begin
+end architecture;
---
+++ test/unit_tests/libraryList/libraryList.test.ts
@@ -0,0 +1,25 @@
+
+import { expect, test } from '@jest/globals';
+import { readdirSync, readFileSync } from 'fs';
+import { join } from 'path';
+import { ProjectParser } from '../../../lib/project-parser';
+import { defaultSettingsGetter } from '../../../lib/settings';
+import { VhdlLinter } from '../../../lib/vhdl-linter';
+
+const files = readdirSync(__dirname).filter(file => file.endsWith('.vhd'));
+test.each(files)('testing library list %s', async (file: string) => {
+  const path = join(__dirname, file);
+  const projectParser = await ProjectParser.create([__dirname], '', defaultSettingsGetter);
+
+  const linter = new VhdlLinter(path, readFileSync(path, { encoding: 'utf8' }),
+    projectParser, defaultSettingsGetter);
+  
+  expect(linter.file).toBeDefined();
+  expect(linter.file.entities).toHaveLength(1);
+  expect(linter.file.entities[0].libraries.map(l => l.lexerToken.getLText())).toContain('std'); // default lib
+  expect(linter.file.entities[0].libraries.map(l => l.lexerToken.getLText())).toContain('work'); // default lib
+  expect(linter.file.entities[0].libraries.map(l => l.lexerToken.getLText())).toContain('ieee');
+  expect(linter.file.entities[0].libraries.map(l => l.lexerToken.getLText())).toContain('test');
+
+  await projectParser.stop();
+});
