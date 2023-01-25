91d82386a44e8f888b3ab5fd88907fee9284ab0e
anton schulte <mail@anton-schulte.de>
2023-01-25 14:58:08 +0100

    Improved handling of assignment edge cases

---
+++ test/test_files/test_error_expected/01/test_assignment_error3.vhd
@@ -0,0 +1,13 @@
+-- Testing multiple lesser used optionals of the assignment syntax
+entity test_assignment is
+
+end test_assignment;
+
+architecture arch of test_assignment is
+  signal target : integer;  -- vhdl-linter-disable-line unused
+begin
+  dummy_proc : process
+  begin
+    target <= guarded 5;
+  end process;
+end architecture;
