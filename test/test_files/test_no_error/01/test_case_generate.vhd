fc1fdde88214f807ba07cb25b69e14e7c5d3fbd8
anton schulte <mail@anton-schulte.de>
2023-01-25 16:57:29 +0100

    fixed vuint

--- test/test_files/test_no_error/01/test_case_generate.vhd
+++ test/test_files/test_no_error/01/test_case_generate.vhd
@@ -1,31 +1,34 @@
 library ieee;
 use ieee.std_logic_1164.all;
 entity test_nested_generate is
+  generic (
+    A_UNUSED : std_ulogic
+    );
 end test_nested_generate;
 architecture arch of test_nested_generate is
 
-  signal a_unused : std_ulogic;
 begin
   gen :
-    case a_unused generate
+    case A_UNUSED generate
     when a_label : '0' =>
-      null;
+
     when b_label : '1' =>
   begin
-    null;
+
     when c_label : 'Z' =>
-    begin
-      null;
-    end c_label;
+      begin
+
+      end c_label;
     when d_label : 'H' =>
-    begin
-      null;
-    end;
+      begin
+
+      end;
     when 'L' =>
-    end;
-  end generate gen;
-  gen2 :
+    when others =>
+  end;
+end generate gen;
+gen2 :
   for i_unused in 0 to 5 generate
   end generate gen2;
 
-end arch;
+  end arch;
