652715afd12b090e1c1993707fb1426b0c810c37
anton schulte <mail@anton-schulte.de>
2023-01-27 15:41:40 +0100

    improved testing

--- test/unit_tests/definition/definition.test.ts
+++ test/unit_tests/definition/definition.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@jest/globals';
+import { beforeAll, expect, test } from '@jest/globals';
 import { readFileSync } from 'fs';
 import { Position } from 'vscode-languageserver';
 import { Elaborate } from '../../../lib/elaborate/elaborate';
@@ -6,19 +6,27 @@ import { findDefinitions } from '../../../lib/languageFeatures/findDefinition';
 import { ProjectParser } from '../../../lib/project-parser';
 import { defaultSettingsGetter } from '../../../lib/settings';
 import { VhdlLinter } from '../../../lib/vhdl-linter';
-
-test(`Testing definitions`, async () => {
-  const projectParser = await ProjectParser.create([__dirname], '', defaultSettingsGetter);
-  const linter = new VhdlLinter('definition.vhd', readFileSync(__dirname + '/definition.vhd', { encoding: 'utf8' }),
+let linter: VhdlLinter;
+let projectParser: ProjectParser;
+beforeAll(async () => {
+  projectParser = await ProjectParser.create([__dirname], '', defaultSettingsGetter);
+  linter = new VhdlLinter('definition.vhd', readFileSync(__dirname + '/definition.vhd', { encoding: 'utf8' }),
     projectParser, defaultSettingsGetter);
   await Elaborate.elaborate(linter);
+  await projectParser.stop();
+});
+test(`Testing definitions`, async () => {
   for (const character of [13, 14, 15]) {
     const definition = await findDefinitions(linter, Position.create(12, character));
     expect(definition).toHaveLength(1);
     expect(definition[0].targetUri.replace(__dirname, '')).toBe('file:///definition.vhd');
     expect(definition[0].targetRange.start.line).toBe(7);
     expect(definition[0].targetRange.end.line).toBe(7);
-    await projectParser.stop();
-
   }
+});
+test(`Testing empty definitions`, async () => {
+  const definition = await findDefinitions(linter, Position.create(16, 0));
+  expect(definition).toHaveLength(0);
+  const definition2 = await findDefinitions(linter, Position.create(6, 0));
+  expect(definition2).toHaveLength(0);
 });
