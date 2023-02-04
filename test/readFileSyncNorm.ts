8dc5cb864b7928bd5ab92d9fc621c8716f13b07b
Anton Schulte <a>
2023-02-04 20:23:47 +0100

    lint

--- test/readFileSyncNorm.ts
+++ test/readFileSyncNorm.ts
@@ -1,5 +1,8 @@
-import { readFileSync } from 'fs';
+import { PathOrFileDescriptor, readFileSync } from 'fs';
 
-export function readFileSyncNorm(path: any, options: any) {
+export function readFileSyncNorm(path: PathOrFileDescriptor, options: {
+  encoding?: null | undefined;
+  flag?: string | undefined;
+}) {
   return readFileSync(path, options).toString().replaceAll('\r\n', '\n');
 }
