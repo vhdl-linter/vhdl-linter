3bf58f64ba03def15bccbdc003a1128ff7e818ed
Your Name <you@example.com>
2023-02-05 11:32:53 +0100

    fix non win

--- test/unit_tests/urlHandling/URL.test.ts
+++ test/unit_tests/urlHandling/URL.test.ts
@@ -2,13 +2,20 @@ import { expect, test } from '@jest/globals';
 import { fileURLToPath, pathToFileURL } from 'url';
 import { normalizeUri } from '../../../lib/normalize-uri';
 
-const example = `file:///c%3A/Users/anton/Documents/vhdl-linter/test/unit_tests/rename/test%20a/architecture_split.vhd`
-const examplePath = `c:\\Users\\anton\\Documents\\vhdl-linter\\test\\unit_tests\\rename\\test a\\architecture_split.vhd`;
+let example: string, examplePath: string;
+if (process.platform === "win32") {
+    example = `file:///c%3A/Users/anton/Documents/vhdl-linter/test/unit_tests/rename/test%20a/architecture_split.vhd`;
+    examplePath = `c:\\Users\\anton\\Documents\\vhdl-linter\\test\\unit_tests\\rename\\test a\\architecture_split.vhd`;
+} else {
+    example = `file:///testPath/test%20a/architecture_split.vhd`;
+    examplePath = `/testPath/test a/architecture_split.vhd`;
+
+}
 test('tesing node URL', () => {
     const normalizedURL = normalizeUri(example);
     const url = new URL(normalizedURL);
     const path = fileURLToPath(url);
-    expect(path).toEqual(examplePath)
+    expect(path).toEqual(examplePath);
     // expect(url.pathname).toEqual(examplePath); // Thats the wrong one
     expect(pathToFileURL(path).toString()).toBe(normalizedURL);
 });
