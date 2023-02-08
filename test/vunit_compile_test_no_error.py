3468aaa95c8c93780af5879e38510c41e693622f
anton schulte <mail@anton-schulte.de>
2023-02-08 23:35:21 +0100

    osvvm ignore empty folder

--- test/vunit_compile_test_no_error.py
+++ test/vunit_compile_test_no_error.py
@@ -10,6 +10,6 @@ for dir in Path(__file__).parent.joinpath('test_files', 'test_no_error').iterdir
     libraryname = 'OSVVM';
   vu.add_library(libraryname)
 
-  vu.add_source_files(dir.absolute() / '[!_]*.vhd', libraryname, vhdl_standard='2008')
+  vu.add_source_files(dir.absolute() / '[!_]*.vhd', libraryname, vhdl_standard='2008', allow_empty=True)
 
 vu.main()
