f98c57748f4df1b3349cc2101659876e8edb0908
g0t00 <www.gollum@gmail.com>
2023-02-15 22:40:41 +0100

    improved testing of attribute parser (#263)
    * improved testing
    
    * Update test/unit_tests/attributeParser/attributeParser.test.ts
    
    Co-authored-by: Niklas <Nik-Sch@users.noreply.github.com>
    
    * address PR
    
    * cleanup merge
    
    * comment regarding PR
    
    * added quickfix
    
    * Update lib/parser/parser-base.ts
    
    Co-authored-by: Niklas <Nik-Sch@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: anton schulte <mail@anton-schulte.de>
    Co-authored-by: Niklas <Nik-Sch@users.noreply.github.com>

---
+++ test/unit_tests/attributeParser/attribute_test_error.vhd
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ent is
+
+end ent;
+
+architecture arch of ent is
+
+  function funky return boolean;
+  function funky return integer;
+
+
+  attribute PIN_NO of funky[  : signal is 5;
+
+begin
+
+end architecture;
+
+entity adder is
+end entity;
---
+++ test/unit_tests/attributeParser/attribute_test_error3.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ent is
+
+end ent;
+
+architecture arch of ent is
+
+
+  attribute PIN_NO :;
+
+
+begin
+
+end architecture;
+
+entity adder is
+end entity;
