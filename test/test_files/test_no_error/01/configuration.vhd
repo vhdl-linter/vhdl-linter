f0c3164746fd7ead0ef073c51b46bbaf6564a2c9
Nik-Sch <mail@niklas-schelten.de>
2023-02-16 10:59:35 +0100

    fixed vunit

--- test/test_files/test_no_error/01/configuration.vhd
+++ test/test_files/test_no_error/01/configuration.vhd
@@ -1,5 +1,5 @@
 -- regression test to verify that files with only configurations are not empty (#270)
-configuration dut_cfg of dut is
-	for rtl
+configuration dut_cfg of for_test is
+	for arch
 	end for;
 end configuration;
