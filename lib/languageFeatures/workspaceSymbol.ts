5a838e55509a0c10e942f4711f15ebe773dd2e49
anton schulte <mail@anton-schulte.de>
2023-02-24 14:37:13 +0100

    address pr

--- lib/languageFeatures/workspaceSymbol.ts
+++ lib/languageFeatures/workspaceSymbol.ts
@@ -3,11 +3,10 @@ import { SymbolInformation, SymbolKind, WorkspaceSymbolParams } from 'vscode-lan
 import { OProcess, OSubprogram, OPackage, OEntity, OType, ORecordChild } from '../parser/objects';
 import { ProjectParser } from '../projectParser';
 
-export function workspaceSymbol(params: WorkspaceSymbolParams, projectParser: ProjectParser, additionPaths: string[]): SymbolInformation[] | null {
-  const start = Date.now();
+export function workspaceSymbol(params: WorkspaceSymbolParams, projectParser: ProjectParser, additionalPaths: string[]): SymbolInformation[] | null {
   const symbols: SymbolInformation[] = [];
   for (const cachedFile of projectParser.cachedFiles) {
-    if (cachedFile.builtIn || additionPaths.find(additionalPath => fileURLToPath(cachedFile.uri).startsWith(additionalPath))) {
+    if (cachedFile.builtIn || additionalPaths.find(additionalPath => fileURLToPath(cachedFile.uri).startsWith(additionalPath))) {
       continue;
     }
     for (const object of cachedFile.linter.file.objectList) {
@@ -29,6 +28,5 @@ export function workspaceSymbol(params: WorkspaceSymbolParams, projectParser: Pr
     }
   }
   const symbolsFiltered = symbols.filter(symbol => symbol.name.toLowerCase().includes(params.query.toLowerCase()));
-  console.log('Workspace Symbol ', Date.now() - start);
   return symbolsFiltered;
 }
