9c717df6f20538799a466f25a23cd51532ad0086
anton schulte <mail@anton-schulte.de>
2023-02-24 18:09:12 +0100

    i give you testcase

---
+++ test/test_files/test_no_error/attribute_reference/record_literal_attribute.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+package record_literal_attribute is
+  type t_rec is record
+    prop : std_ulogic_vector;
+  end type;
+end package;
+
+package body record_literal_attribute is
+  function InitAxi4LiteReadAddressRec (foo : std_ulogic_vector)  -- fo is read via attribute reference
+    return t_rec is
+  begin
+    return (
+      prop => (foo'range => '0')
+      );
+  end function InitAxi4LiteReadAddressRec;
+end package body;
