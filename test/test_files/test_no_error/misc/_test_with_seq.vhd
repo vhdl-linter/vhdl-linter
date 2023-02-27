863e8efc1be8264a73a8911d2e305019c670bd3d
anton schulte <mail@anton-schulte.de>
2023-02-27 23:48:04 +0100

    ghdl is wrong

--- test/test_files/test_no_error/misc/_test_with_seq.vhd
+++ test/test_files/test_no_error/misc/_test_with_seq.vhd
@@ -7,12 +7,16 @@ architecture arch of test_with is
 	signal a : std_ulogic_vector(2 - 1 downto 0);  -- vhdl-linter-disable-line unused
 	signal b : std_ulogic_vector(4 - 1 downto 0);  -- vhdl-linter-disable-line unused
 begin
-	with a select b <=
-		"1000" when "00",
-		"0100" when "01",
-		"0010" when "10",
-		"0001" when "11",
-		"0001" when others;
 
+
+	process is
+	begin
+		with a select b <=
+			"1000" when "00",
+			"0100" when "01",
+			"0010" when "10",
+			"0001" when "11",
+			"0001" when others;
+	end process;
 
 end architecture;
