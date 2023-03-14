9964c75278b682c3b23b237c8dd19a7ffb880478
g0t00 <www.gollum@gmail.com>
2023-03-14 14:26:34 +0100

    multi threaded test runner (#319)
    * multi threaded test runner
    
    * queue
    
    * optimizing
    
    * undo npm i
    
    * redo install
    
    * cleanup
    
    * threadnum cpu/2
    
    ---------
    
    Co-authored-by: anton schulte <mail@anton-schulte.de>

---
+++ test/testUtil.ts
@@ -0,0 +1,43 @@
+import { readdirSync } from "fs";
+import { DiagnosticSeverity } from "vscode-languageserver";
+import { OIRange } from "../lib/parser/objects";
+import { joinURL } from "../lib/projectParser";
+import { OIDiagnostic } from "../lib/vhdlLinter";
+
+export interface MessageWrapper {
+  file: string,
+  messages: (OIDiagnostic | { message: string })[]
+}
+function isOIDiagnostic(obj: unknown): obj is OIDiagnostic {
+  if ((obj as OIDiagnostic).range instanceof OIRange) {
+    return true;
+  }
+  return false;
+}
+export function readDirPath(path: URL) {
+  return readdirSync(path).map(file => joinURL(path, file));
+}
+function getMessageColor(message: OIDiagnostic | { message: string }) {
+  if (isOIDiagnostic(message) && message.severity === DiagnosticSeverity.Error) {
+    return '\u001b[31m';
+  } else if (isOIDiagnostic(message) && message.severity === DiagnosticSeverity.Warning) {
+    return '\u001b[33m';
+  }
+  return '\u001b[34m';
+}
+export function prettyPrintMessages(messages: MessageWrapper[]) {
+  return messages.map(message => {
+    const filename = message.file.replace(cwd(), '');
+    return message.messages.slice(0, 5).map((innerMessage) => {
+      const messageText = `${getMessageColor(innerMessage)}${innerMessage.message}\u001b[0m`;
+      if (isOIDiagnostic(innerMessage)) {
+        return `${filename}:${innerMessage.range.start.line + 1} (r: ${innerMessage.range.start.line}:${innerMessage.range.start.character} - ${innerMessage.range.end.line}:${innerMessage.range.end.character})\n  ${messageText}`; // lines are 0 based in OI
+      }
+      return `${filename}\n  ${messageText}`;
+    }).join('\n') + (message.messages.length > 5 ? `\n\u001b[31m ... and ${message.messages.length - 5} more\u001b[0m` : '');
+  }).join('\n');
+}
+
+function cwd(): { [Symbol.replace](string: string, replaceValue: string): string; } {
+  throw new Error("Function not implemented.");
+}
---
+++ test/testWorker.ts
@@ -0,0 +1,67 @@
+import { lstatSync } from "fs";
+import { argv } from "process";
+import { parentPort, workerData } from "worker_threads";
+import { ProjectParser } from "../lib/projectParser";
+import { defaultSettingsGetter, defaultSettingsWithOverwrite } from "../lib/settings";
+import { VhdlLinter } from "../lib/vhdlLinter";
+import { readFileSyncNorm } from "./readFileSyncNorm";
+import { MessageWrapper, prettyPrintMessages, readDirPath } from "./testUtil";
+async function run_test(path: URL, error_expected: boolean, projectParser?: ProjectParser): Promise<MessageWrapper[]> {
+  const messageWrappers: MessageWrapper[] = [];
+  if (!projectParser) {
+    projectParser = await ProjectParser.create([path], '', defaultSettingsGetter);
+  }
+  for (const subPath of readDirPath(path)) {
+    if (argv.includes('--no-osvvm') && subPath.pathname.match(/OSVVM/i)) {
+      continue;
+    }
+    // Exclude OSVVM and IEEE from some checker
+    const getter = subPath.pathname.match(/OSVVM/i) || subPath.pathname.match(/ieee/i)
+      ? defaultSettingsWithOverwrite({
+        rules: {
+          "type-resolved": false,
+          "unit": false
+        }
+      })
+      : defaultSettingsGetter;
+    if (lstatSync(subPath).isDirectory()) {
+      messageWrappers.push(...await run_test(subPath, error_expected, projectParser));
+    } else if (subPath.pathname.match(/\.vhdl?$/i)) {
+      const text = readFileSyncNorm(subPath, { encoding: 'utf8' });
+      const vhdlLinter = new VhdlLinter(subPath, text, projectParser, getter);
+      if (vhdlLinter.parsedSuccessfully) {
+        await vhdlLinter.checkAll();
+      }
+      if (error_expected === false) {
+        if (vhdlLinter.messages.length > 0) {
+          const newMessage = {
+            file: subPath.pathname,
+            messages: vhdlLinter.messages
+          };
+          messageWrappers.push(newMessage);
+          console.log(prettyPrintMessages([newMessage]));
+        }
+      } else {
+        if (vhdlLinter.messages.length !== 1) {
+          const newMessage = {
+            file: subPath.pathname,
+            messages: [...vhdlLinter.messages, { message: `One message expected found ${vhdlLinter.messages.length}` }]
+          };
+          messageWrappers.push(newMessage);
+          console.log(prettyPrintMessages([newMessage]));
+
+        }
+      }
+
+
+    }
+  }
+
+  return messageWrappers;
+}
+(async () => {
+  const { path, error_expected } = workerData as { path: string, error_expected: boolean };
+  const messages = await run_test(new URL(path), error_expected);
+
+  parentPort?.postMessage(messages);
+})().catch(err => console.error(err));
