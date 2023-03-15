aa5b832c5b823c6d8dd6ed21a1372c706291043f
g0t00 <www.gollum@gmail.com>
2023-03-15 12:09:42 +0100

    Update test/test_files/test_error_expected/type_checking/pkg_in_array.vhd
    Co-authored-by: Niklas <Nik-Sch@users.noreply.github.com>

--- test/test_files/test_error_expected/type_checking/pkg_in_array.vhd
+++ test/test_files/test_error_expected/type_checking/pkg_in_array.vhd
@@ -2,7 +2,7 @@
 entity pkg_in_port_map is
 end entity;
 architecture arch of pkg_in_port_map is
-  type test_unused is array (positive) of work.dummy_pkg;
+  type test_unused is array (positive) of work.dummy_pkg; -- array of package is not allowed
 begin
 
 end architecture;
