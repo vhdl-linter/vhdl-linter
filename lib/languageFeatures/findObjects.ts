7f62f803e3e0d2eba70e95394fc702982bd21561
Nik-Sch <mail@niklas-schelten.de>
2023-03-21 09:58:55 +0100

    fix lint

--- lib/languageFeatures/findObjects.ts
+++ lib/languageFeatures/findObjects.ts
@@ -1,7 +1,7 @@
 import { Position } from "vscode-languageserver";
 import { OLexerToken } from "../lexer";
 import { implementsIHasEndingLexerToken, implementsIHasLexerToken } from "../parser/interfaces";
-import { OArchitecture, OAssociation, ObjectBase, OComponent, OConfigurationDeclaration, OInstantiation, OName } from "../parser/objects";
+import { OArchitecture, OAssociation, ObjectBase, OComponent, OConfigurationDeclaration, OName } from "../parser/objects";
 import { VhdlLinter } from "../vhdlLinter";
 import { SetAdd } from "./findReferencesHandler";
 
