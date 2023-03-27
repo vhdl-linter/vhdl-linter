b8d8db57c0fd37e80cefc84126306a53b7fc239a
anton schulte <mail@anton-schulte.de>
2023-03-27 11:22:01 +0200

    reworked rule instantiations

---
+++ test/test_files/test_error_expected/instantiation/test_instantiation.vhd
@@ -0,0 +1,20 @@
+-- vhdl-linter-disable unused
+entity test_instantiation is
+end entity;
+architecture arch of test_instantiation is
+  procedure getBar(a, b, c, d : integer) is
+  begin
+  end procedure;
+  procedure getFoo(a : integer; b : integer := 5) is
+  begin
+  end procedure;
+  procedure getFoobar(a : integer; b : integer := 5; c : integer) is
+  begin
+  end procedure;
+  signal apfel : integer;
+begin
+  p_label : process is
+  begin
+    getBar(0, b => 1, 2, 3);
+  end process;
+end architecture;
