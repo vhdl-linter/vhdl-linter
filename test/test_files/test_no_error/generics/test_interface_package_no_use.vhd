4f80d37d5ae103f8534e222a000e48fbe775c57c
Nik-Sch <mail@niklas-schelten.de>
2023-04-03 17:17:40 +0200

    address review

--- test/test_files/test_no_error/generics/test_interface_package_no_use.vhd
+++ test/test_files/test_no_error/generics/test_interface_package_no_use.vhd
@@ -1,6 +1,6 @@
 entity test_entity_generic_no_use is
 generic (
-  package test_pkg is new work.generic_pkg generic map (<>) -- test_pkg is not used
+  package test_pkg is new work.generic_pkg generic map (<>) -- test_pkg should be used
   );
 end entity;
 architecture arch of test_entity_generic_no_use is
--- test/test_files/test_no_error/generics/test_package_inst_use_signal.vhd
+++ test/test_files/test_no_error/generics/test_package_inst_use_signal.vhd
@@ -2,6 +2,6 @@ entity test_entity_generic_no_use is
 end entity;
 architecture arch of test_entity_generic_no_use is
   package test_pkg is new work.generic_pkg generic map (generic_parameter => 2); -- test_pkg should be used
-  use test_pkg.all;
+  signal foo_unused : test_pkg.t_testData;
 begin
 end arch;
