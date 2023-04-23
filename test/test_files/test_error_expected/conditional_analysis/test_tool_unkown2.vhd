7b11484684063f0ffd37bb7774683c246481ffea
g0t00 <www.gollum@gmail.com>
2023-04-23 10:30:26 +0200

    Update test/test_files/test_error_expected/conditional_analysis/test_tool_unkown2.vhd
    Co-authored-by: Niklas <Nik-Sch@users.noreply.github.com>

--- test/test_files/test_error_expected/conditional_analysis/test_tool_unkown2.vhd
+++ test/test_files/test_error_expected/conditional_analysis/test_tool_unkown2.vhd
@@ -3,7 +3,7 @@ end entity;
 architecture arch of test_tool_unknown is
 begin
   `if TOOL /= "ASD" then -- This will be true
-  -- This is unkown shall throw info but not kill the parser
+  -- This is unknown shall throw info but not kill the parser
   `UNKNOWN TOOL DIRECTIVE
   `end if
 end architecture;
