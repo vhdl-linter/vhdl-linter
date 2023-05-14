8b6d1bd35e41e8c4de426694db08b3ddd001bccc
Nik-Sch <mail@niklas-schelten.de>
2023-05-14 17:42:33 +0200

    mwe

---
+++ test/test_files/test_no_error/selected_names/range_selected_name.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+entity range_selected_name is
+end entity;
+architecture rtl of range_selected_name is
+  type test is record
+    element: std_ulogic_vector(1 downto 0);
+  end record;
+  signal rec: test;
+  signal s: std_ulogic_vector(rec.element'range);
+begin
+  rec.element <= s;
+  s <= rec.element;
+end architecture;
