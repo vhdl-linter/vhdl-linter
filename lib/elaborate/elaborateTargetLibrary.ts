b737f1e340601166a7eb7c81b0096f80e66c29bb
Nik-Sch <mail@niklas-schelten.de>
2023-06-24 17:43:49 +0200

    no info when not using target library

--- lib/elaborate/elaborateTargetLibrary.ts
+++ lib/elaborate/elaborateTargetLibrary.ts
@@ -9,14 +9,6 @@ export class ElaborateTargetLibrary {
   public elaborate() {
     // checks if either a magic comment or a library mapping defines a library for this file and saves it
     const fileTargetLibrary = this.vhdlLinter.projectParser.libraryMap.get(this.vhdlLinter.file.uri.toString());
-    // no library at all defined
-    if (fileTargetLibrary === undefined && this.vhdlLinter.file.targetLibrary === undefined) {
-      this.vhdlLinter.addMessage({
-        message: `This file has no target library assigned.`,
-        range: new OIRange(this.vhdlLinter.file, 0, this.vhdlLinter.file.lines[0]?.length ?? 1),
-        severity: DiagnosticSeverity.Information,
-      }, 'elaborate');
-    }
     // two libraries defined
     if (this.vhdlLinter.file.targetLibrary !== undefined && fileTargetLibrary !== undefined && this.vhdlLinter.file.targetLibrary !== fileTargetLibrary?.library) {
       this.vhdlLinter.addMessage({
