02cef64f0fbf26a9e9a02ac86d24efff920a9b5b
Nik-Sch <mail@niklas-schelten.de>
2023-06-24 18:01:59 +0200

    relative paths

--- lib/elaborate/elaborateTargetLibrary.ts
+++ lib/elaborate/elaborateTargetLibrary.ts
@@ -12,7 +12,7 @@ export class ElaborateTargetLibrary {
     // two libraries defined
     if (this.vhdlLinter.file.targetLibrary !== undefined && fileTargetLibrary !== undefined && this.vhdlLinter.file.targetLibrary !== fileTargetLibrary?.library) {
       this.vhdlLinter.addMessage({
-        message: `The library assigned in the magic comment (${this.vhdlLinter.file.targetLibrary}) does not match ${fileTargetLibrary.library} from ${fileURLToPath(fileTargetLibrary.definitionFile)}:${fileTargetLibrary.definitionLine}.`,
+        message: `The library assigned in the magic comment (${this.vhdlLinter.file.targetLibrary}) does not match ${fileTargetLibrary.library} from ${this.vhdlLinter.projectParser.relativeToWorkspace(fileTargetLibrary.definitionFile)}:${fileTargetLibrary.definitionLine}.`,
         range: new OIRange(this.vhdlLinter.file, 0, this.vhdlLinter.file.lines[0]?.length ?? 1),
         severity: DiagnosticSeverity.Warning,
       }, 'elaborate');
