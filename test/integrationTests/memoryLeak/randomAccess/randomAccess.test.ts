a885c0f6eea8eac9a79e83187054e81278fab8bc
anton schulte <mail@anton-schulte.de>
2023-06-25 14:33:10 +0200

    cleanup

--- test/integrationTests/memoryLeak/randomAccess/randomAccess.test.ts
+++ test/integrationTests/memoryLeak/randomAccess/randomAccess.test.ts
@@ -2,9 +2,8 @@ import { jest, test } from '@jest/globals';
 import { randomTest } from './randomTest';
 import { readDirPath } from '../../../testUtil';
 import { fileURLToPath, pathToFileURL } from 'url';
-import {  statSync } from 'fs';
-jest.setTimeout(10000);
+import { statSync } from 'fs';
+jest.setTimeout(10 * 1000);
 test.each(readDirPath(pathToFileURL(__dirname)).filter(url => statSync(url).isDirectory()))('memory leak test with random access for folder %s', async (folder: URL) => {
   await randomTest(fileURLToPath(folder));
 });
-jest.setTimeout(120000);
