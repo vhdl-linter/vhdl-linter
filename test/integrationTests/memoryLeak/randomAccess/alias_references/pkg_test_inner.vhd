fa88e3054f6fc857aa2b08d4c9b36e7ad38810b7
g0t00 <www.gollum@gmail.com>
2023-06-25 20:49:41 +0200

    More memory leaks (#417)
    * Found more memory leaks
    
    * cleanup
    
    * increase timeout
    
    ---------
    
    Co-authored-by: anton schulte <mail@anton-schulte.de>

---
+++ test/integrationTests/memoryLeak/randomAccess/alias_references/pkg_test_inner.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package pkg_test_inner is
+
+  type test_record_inner is record
+    foo_inner : integer;
+  end record;
+  type test_record is record
+    foo : test_record_inner;
+  end record;
+
+end package;
