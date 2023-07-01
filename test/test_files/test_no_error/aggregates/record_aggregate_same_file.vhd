4990cfd2d0e315fcd14fcf741ade39013ac11564
g0t00 <www.gollum@gmail.com>
2023-07-01 13:15:54 +0200

    Apply suggestions from code review
    Co-authored-by: Niklas <Nik-Sch@users.noreply.github.com>

--- test/test_files/test_no_error/aggregates/record_aggregate_same_file.vhd
+++ test/test_files/test_no_error/aggregates/record_aggregate_same_file.vhd
@@ -5,14 +5,14 @@ end entity;
 architecture arch of record_aggregate_same_file is
   constant DATA_WIDTH : integer := 8;
   type t_axi_stream is record
-    tvalid : std_ulogic;                --! valid
-    tdata  : std_ulogic_vector;  --! data, only complete bytes supported as bit width
-    tstrb  : std_ulogic_vector;         --! data strobe
-    tkeep  : std_ulogic_vector;         --! keep
-    tlast  : std_ulogic;                --! last, indicate packet boundary
-    tid    : std_ulogic_vector;  --! id to identify different data streams
-    tdest  : std_ulogic_vector;  --! routing information for the data stream
-    tuser  : std_ulogic_vector;         --! user sideband data
+    tvalid : std_ulogic;
+    tdata  : std_ulogic_vector;
+    tstrb  : std_ulogic_vector;
+    tkeep  : std_ulogic_vector;
+    tlast  : std_ulogic;
+    tid    : std_ulogic_vector;
+    tdest  : std_ulogic_vector;
+    tuser  : std_ulogic_vector;
   end record t_axi_stream;
   signal foo : t_axi_stream(tdata(DATA_WIDTH - 1 downto 0), tkeep(DATA_WIDTH/8 -1 downto 0), tstrb(-1 downto 0), tid(-1 downto 0), tuser(-1 downto 0), tdest(0 downto 0));
 
--- test/test_files/test_no_error/aggregates/record_definition.vhd
+++ test/test_files/test_no_error/aggregates/record_definition.vhd
@@ -2,13 +2,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 package record_definition is
   type t_axi_stream is record
-    tvalid : std_ulogic;                --! valid
-    tdata  : std_ulogic_vector;  --! data, only complete bytes supported as bit width
-    tstrb  : std_ulogic_vector;         --! data strobe
-    tkeep  : std_ulogic_vector;         --! keep
-    tlast  : std_ulogic;                --! last, indicate packet boundary
-    tid    : std_ulogic_vector;  --! id to identify different data streams
-    tdest  : std_ulogic_vector;  --! routing information for the data stream
-    tuser  : std_ulogic_vector;         --! user sideband data
+    tvalid : std_ulogic;
+    tdata  : std_ulogic_vector;
+    tstrb  : std_ulogic_vector;
+    tkeep  : std_ulogic_vector;
+    tlast  : std_ulogic;
+    tid    : std_ulogic_vector;
+    tdest  : std_ulogic_vector;
+    tuser  : std_ulogic_vector;
   end record t_axi_stream;
 end package;
