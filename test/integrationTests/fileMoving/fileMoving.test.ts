d761af45e2aa45cf371012691a0baea07ddec7ab
anton schulte <mail@anton-schulte.de>
2023-07-08 23:49:35 +0200

    address PR

--- test/integrationTests/fileMoving/fileMoving.test.ts
+++ test/integrationTests/fileMoving/fileMoving.test.ts
@@ -29,6 +29,9 @@ beforeEach(async () => {
   await writeFile(join(__dirname, 'testfiles/test_entity.vhd'), `
       entity test_entity is
       end entity;`);
+  await writeFile(join(__dirname, 'testfiles/test_module.sv'), `
+      module test_module
+      endmodule;`);
 });
 test('testing moving of vhdl files', async () => {
   const oldFileURL = pathToFileURL(join(__dirname, 'testfiles/test_entity.vhd'));
@@ -64,6 +67,43 @@ test('testing moving of vhdl files', async () => {
   ]);
 
   expect(projectParser.entities.filter(entity => entity.lexerToken.getLText() === 'test_entity')).toHaveLength(1);
+
+  await projectParser.stop();
+});
+test('testing moving of sv files', async () => {
+  const oldFileURL = pathToFileURL(join(__dirname, 'testfiles/test_module.sv'));
+  const newFileURL = pathToFileURL(join(__dirname, 'testfiles/test_module_new.sv'));
+
+  const projectParser = await ProjectParser.create([pathToFileURL(__dirname)], defaultSettingsGetter);
+
+  expect(projectParser.entities.filter(entity => entity.lexerToken.getLText() === 'test_module')).toHaveLength(1);
+  await Promise.all([
+    (async () => {
+      await wait(100);
+      await rename(oldFileURL, newFileURL);
+
+    })(),
+    new Promise<void>(resolve => {
+      const handler = (type: string, path: string) => {
+        if (type == 'unlink' && path === oldFileURL.toString()) {
+          projectParser.events.off('change', handler);
+          resolve();
+        }
+      };
+      projectParser.events.on('change', handler);
+    }),
+    new Promise<void>(resolve => {
+      const handler = (type: string, path: string) => {
+        if (type == 'add' && path === newFileURL.toString()) {
+          projectParser.events.off('change', handler);
+          resolve();
+        }
+      };
+      projectParser.events.on('change', handler);
+    })
+  ]);
+
+  expect(projectParser.entities.filter(entity => entity.lexerToken.getLText() === 'test_module')).toHaveLength(1);
 
   await projectParser.stop();
 });
