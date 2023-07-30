f095145294a1bff4e3b15321c84523163ade7617
anton schulte <mail@anton-schulte.de>
2023-07-30 14:28:38 +0200

    Fix test

--- test/integrationTests/testPackage/testPackage.ts
+++ test/integrationTests/testPackage/testPackage.ts
@@ -12,5 +12,21 @@ chdir(testPath);
 const packagePath = readFileSync(join(__dirname, '..', '..', '..', 'filename'), 'utf-8');
 execSync(`npm init -y`);
 execSync(`npm install ${join(__dirname, '..', '..', '..', packagePath)}`);
-// TODO catch correct output
-execSync(`npx --package @vhdl-linter/vhdl-linter vhdl-linter-cli`);
+
+let err: unknown;
+try {
+  execSync(`npx  vhdl-linter-cli ../vhdlFolder -j`, { encoding: 'utf8' });
+} catch (_err: unknown) {
+  err = _err;
+}
+assert(err instanceof Error);
+const stdout = (err as Partial<VhdlLinterCliError>)?.stdout;
+assert(typeof stdout === 'string');
+console.log(stdout);
+const result = JSON.parse(stdout) as { description: string }[];
+assert(result.length === 1);
+assert(result[0]?.description === `Not using signal 'a' (unused)`);
+
+interface VhdlLinterCliError {
+  stdout: string;
+}
