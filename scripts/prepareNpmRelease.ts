debb41d405bc9d6f29dc2b39b431ed717f3da24b
Nik-Sch <mail@niklas-schelten.de>
2023-08-06 13:54:18 +0200

    use version from env

--- scripts/prepareNpmRelease.ts
+++ scripts/prepareNpmRelease.ts
@@ -1,4 +1,3 @@
-import { execSync } from "child_process";
 import { readFileSync, writeFileSync } from "fs";
 
 interface PackageJson {
@@ -9,6 +8,5 @@ interface PackageJson {
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const content: PackageJson = JSON.parse(readFileSync('package.json', { encoding: 'utf8' }));
 content.name = "@vhdl-linter/vhdl-linter";
-execSync('git fetch --tags');
-content.version = execSync('git describe --tags --abbrev=0', {encoding: 'utf8'}).trim();
+content.version = process.env.RELEASE_VERSION;
 writeFileSync('package.json', JSON.stringify(content, undefined, 2));
