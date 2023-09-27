173d28c506d45943160d2e8f95dfc8caa0f56dfa
g0t00 <www.gollum@gmail.com>
2023-09-27 13:27:41 +0200

    Added (manual) modelsim check (#457)
    * Ran files through Modelsim
    
    * unused
    
    ---------
    
    Co-authored-by: anton schulte <mail@anton-schulte.de>

--- test/test_files/test_no_error/verilog/test_multiple_module.sv
+++ test/test_files/test_no_error/verilog/test_multiple_module.sv
@@ -4,7 +4,7 @@ module test_multiple_module1
 )
 (
 		input  wire [0:0]   in_bit,
-		output wire [0:0]   out_bit, // test comment
+		output wire [0:0]   out_bit // test comment
     );
 endmodule;
 module test_multiple_module2
@@ -13,6 +13,6 @@ module test_multiple_module2
 )
 (
 		input  wire [0:0]   in_bit2,
-		output wire [0:0]   out_bit2, // test comment
+		output wire [0:0]   out_bit2 // test comment
     );
 endmodule;
--- test/vunit_compile_test_no_error.py
+++ test/vunit_compile_test_no_error.py
@@ -9,11 +9,26 @@ for dir in Path(__file__).parent.joinpath("test_files", "test_no_error").iterdir
     print("\n\nentering folder: %s" % (dir.absolute()))
     # Create VUnit instance by parsing command line arguments
     vu.add_library(libraryname)
+    filter = "[!_%]*.vhd"
+    if vu.get_simulator_name() == "modelsim":
+        filter = f"[!$%]*.vhd"
+        vu.set_compile_option('modelsim.vcom_flags', ['-vhpreprocess'], True)
+
     vu.add_source_files(
-        dir.absolute() / "[!_]*.vhd",
+        dir.absolute() / filter,
         libraryname,
         vhdl_standard="2008",
         allow_empty=True,
     )
+    if vu.get_simulator_name() == "modelsim":
+        filter = f"[!$%]*.sv"
+
+        vu.add_source_files(
+            dir.absolute() / filter,
+            libraryname,
+            vhdl_standard="2008",
+            allow_empty=True,
+        )
+    print(vu.get_simulator_name())
 
 vu.main()
