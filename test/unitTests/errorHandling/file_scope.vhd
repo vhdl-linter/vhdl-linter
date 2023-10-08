24d23aeae1b75249391f594e0f1618f6a8c743ed
g0t00 <www.gollum@gmail.com>
2023-10-08 12:05:47 +0200

    Update test/unitTests/errorHandling/file_scope.vhd
    Co-authored-by: Niklas <Nik-Sch@users.noreply.github.com>

--- test/unitTests/errorHandling/file_scope.vhd
+++ test/unitTests/errorHandling/file_scope.vhd
@@ -4,7 +4,7 @@ end entity;
 architecture arch of test1 is
 begin
 end architecture;
-mySignal_out <= '1';  --- A nonsense syntax error at end of file
+mySignal_out <= '1';  -- A nonsense syntax error outside a design entity
 entity test2 is
 end entity;
 architecture arch of test2 is
