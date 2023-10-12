28b94ebbe0b995001db8328137f9f56ae5eb5265
Nik-Sch <mail@niklas-schelten.de>
2023-10-12 18:14:27 +0200

    fix ghdl protected type

--- test/test_files/test_no_error/selected_names/hidden.vhd
+++ test/test_files/test_no_error/selected_names/hidden.vhd
@@ -1,21 +1,26 @@
-package shadowed is
+package hidden is
+
   type protected_type is protected
-    impure function NewID(Name : string) return integer;
+    impure function NewID(name : integer) return integer;
   end protected;
-  shared variable protected_variabe : protected_type;
 
-  ------------------------------------------------------------
-  impure function NewID (
-    Name : string
-    ) return integer;
+  shared variable protected_variable : protected_type;
 
+  impure function NewID(name : integer) return integer;
 end package;
-package body shadowed is
-  impure function NewID (
-    Name : string
-    ) return integer is
+
+package body hidden is
+
+  impure function NewID (name : integer) return integer is
   begin
-    return protected_variabe.NewID(Name);
+    return protected_variable.NewID(name);
   end function NewID;
+
+  type protected_type is protected body
+    impure function NewID(name : integer) return integer is
+    begin
+      return name;
+    end function;
+  end protected body;
 
 end package body;
