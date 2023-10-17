6d05fe286df64e1e22c78943df2f5921f0fb649d
g0t00 <www.gollum@gmail.com>
2023-10-17 08:47:16 +0200

    Fix non-attribute declarations hiding attributes (#484)
    * Fix non-attribute declarations hiding attributes
    
    * Update test/test_files/test_no_error/attribute_reference/hiding.vhd
    
    Co-authored-by: Niklas <Nik-Sch@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: anton schulte <mail@anton-schulte.de>
    Co-authored-by: Niklas <Nik-Sch@users.noreply.github.com>

---
+++ test/test_files/test_no_error/attribute_reference/hiding.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity hiding is
+
+end hiding;
+
+architecture arch of hiding is
+  signal foo : std_ulogic_vector(5 downto 0);  -- vhdl-linter-disable-line unused
+begin
+  test : process
+    variable length : integer;          -- vhdl-linter-disable-line unused
+
+  begin
+    report to_string(foo'length);  -- length should not be hidden by the variable
+  end process;
+end architecture;
